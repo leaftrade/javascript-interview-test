a7d6861545eb340fb38a08d08fa94088c6cd8a88
Ted_M <theodorjemorrison@gmail.com>
2019-03-10 18:57:28 -0500

    Added main source code and supporting jetbrains (.idea) configs

--- test/leaftrade.test.js
+++ test/leaftrade.test.js
@@ -4,18 +4,24 @@ describe('Leaftrade Tests', () => {
     describe('Reverse Array', () => {
         it('should turn the below string into an array and reverse the words', () => {
             let data = "I want this job.";
-
-            // Code here
-
+            datamod = data.replace(/\./g, '');
+            var arr = datamod.split(' ');
+            console.log(arr);
+            data = arr.reverse();
+            console.log(data);
             expect(['job', 'this', 'want', 'I']).to.deep.equal(data);
         });
     });
     describe('Order Array', () => {
         it('should sort the below array', () => {
             let data = ['200', '450', '2.5', '1', '505.5', '2'];
-
-            // Code here
-
+            dcopy = data.sort();
+            var arr = [];
+            for(var i =0; i<dcopy.length; i++)
+                arr.push(Number(dcopy[i]));
+            console.log(arr)
+            //data = '"' + arr.join(' "," ') + '"';
+            data = arr;
             expect([1, 2, 2.5, 200, 450, 505.5]).to.deep.equal(data);
         });
     });
@@ -24,12 +30,35 @@ describe('Leaftrade Tests', () => {
             let data1 = [1, 2, 3, 4, 5, 6, 7];
             let data2 = [2, 4, 5, 7, 8, 9, 10];
 
+            var arr1 = [];
+            var arr2 = [];
+
+            console.log(data1.indexOf(data2[2]));
+
+            /*indexOf returns -1 if the value to search for never occurs.
+            * so this is a janky way of using this builtin but it works pretty well*/
+
+            for(var i = 0; i < data1.length; i++){
+                if(data2.indexOf(data1[i]) === -1) arr1.push(data1[i]);
+            }
+            for(var j = 0; j < data2.length; j++){
+                if(data1.indexOf(data2[j]) === -1) arr2.push(data2[j]);
+            }
+
+            console.log(data1);
+            console.log(arr1);
+            console.log('');
+            console.log(data2);
+            console.log(arr2);
+
+
             // Code here
+            data = arr2;
 
             expect([8, 9, 10]).to.deep.equal(data);
 
             // Code here
-
+            data = arr1;
             expect([1, 3, 6]).to.deep.equal(data);
         });
     });
@@ -44,7 +73,34 @@ describe('Leaftrade Tests', () => {
                 lon: '-88.3429465',
             };
 
-            // Code here
+            let x1 = place1.lat,
+                y1 = place1.lon,
+                x2 = place2.lat,
+                y2 = place2.lon;
+
+            /* I thought looking for a library function for this somewhere in js.
+            * But I'm always worried about losing touch with my academic math, so
+            * haversine formula it is.*/
+
+            /**https://en.wikipedia.org/wiki/Haversine_formula**/
+            var R = 6372; //km of earth radius
+            var delta_x = (x2 - x1) * Math.PI / 180;
+            var delta_y = (y2 - y1) * Math.PI / 180;
+
+            var a = Math.pow((Math.sin(delta_x))/2,2) +
+                (Math.cos(x1 * (Math.PI / 180)) * Math.cos(x2* (Math.PI / 180)) *
+                Math.pow((Math.sin(delta_y))/2,2));
+
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            var d = R * c;  //in kilometers
+
+            var distance_float = Math.round(d* 0.6214 * 100) /100; //in miles
+
+            console.log(distance_float.toString());
+
+            var distance = distance_float.toString()
+
 
             expect(distance).to.equal('36.91');
         });
@@ -55,6 +111,7 @@ describe('Leaftrade Tests', () => {
             let time2 = '2016-06-05T15:00:00';
 
             // Code here
+            /**/
 
             expect(timeDiff).to.equal('3 hours ago');
         });
