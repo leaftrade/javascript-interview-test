fcd3c195d32b70f108eeb5ec97653c33b24bc17b
GoingTharn <john.barry@gmail.com>
2019-08-02 08:23:36 -0500

    Finished tests.
    Notes:
    Test Order Array - it could be more efficient by casting to Number
    whilst sorting, but I challenge the result changing the type of the
    elements and would like to discuss this. (hence adding the traversal
    cast rather than integrating it into the function)
    
    Test Get Diff Array - Set implementation would go elsewhere; it was
    simple enough to patch in here rather than go npm exploring
    
    Test Get Distance - I think the tested result may be wrong; I attempted
    3 different approaches and never got to the same number, and indeed
    didn't get it on websites online.
    Furthermore, the type was wrong and was testing a string when the
    distance would/should be a Number or Float.
    Regardless, since I didn't know the use case, I chose to implement the
    most "accurate" (i.e.  closest to the value provided in the test file)
    and use chai-almost to express the expectation of accuracy, in this case
    to tens of meters.
    
    Cheers.

--- test/leaftrade.test.js
+++ test/leaftrade.test.js
@@ -1,12 +1,21 @@
+const chai = require('chai');
+const chaiAlmost = require('chai-almost');
 const expect = require('chai').expect;
+const GeoPoint = require('geopoint');
+const moment = require('moment');
 
 describe('Leaftrade Tests', () => {
     describe('Reverse Array', () => {
         it('should turn the below string into an array and reverse the words', () => {
             let data = "I want this job.";
-
             // Code here
+            let output = (input) => {
+                // NB this is only suitable for American English.
+                let cleaned = input.replace(/[^a-zA-Z ]+/g, '').replace('/ {2,}/', ' ')
+                return cleaned.split(" ").reverse()
+            };
 
+            data = output(data);
             expect(['job', 'this', 'want', 'I']).to.deep.equal(data);
         });
     });
@@ -15,7 +24,10 @@ describe('Leaftrade Tests', () => {
             let data = ['200', '450', '2.5', '1', '505.5', '2'];
 
             // Code here
-
+            let num_sort = (data) => {
+                return data.sort((a, b) => a - b);
+            };
+            data = num_sort(data).map(Number);
             expect([1, 2, 2.5, 200, 450, 505.5]).to.deep.equal(data);
         });
     });
@@ -25,11 +37,21 @@ describe('Leaftrade Tests', () => {
             let data2 = [2, 4, 5, 7, 8, 9, 10];
 
             // Code here
+            let array_diff = (first, second) => {
+                const setA = new Set(first);
+                const setB = new Set(second);
+                var _difference = new Set(setA);
+                for (const elem of setB) {
+                    _difference.delete(elem);
+                }
+                return Array.from(_difference);
+            };
 
+            let data = array_diff(data2, data1);
             expect([8, 9, 10]).to.deep.equal(data);
 
             // Code here
-
+            data = array_diff(data1, data2);
             expect([1, 3, 6]).to.deep.equal(data);
         });
     });
@@ -45,17 +67,30 @@ describe('Leaftrade Tests', () => {
             };
 
             // Code here
+            let dist_in_miles = (point1, point2) => {
+                var p1 = new GeoPoint(Number(point1.lat), Number(point1.lon));
+                var p2 = new GeoPoint(Number(point2.lat), Number(point2.lon));
+                return p1.distanceTo(p2, false);
+            };
 
-            expect(distance).to.equal('36.91');
+            chai.use(chaiAlmost(.01))
+            // I can come within 10 meters.
+            distance = dist_in_miles(place1, place2);
+            expect(distance).to.almost.eql(36.91);
         });
     });
+
     describe('Get Human Time Diff', () => {
         it('should generate a human readable time difference', () => {
             let time1 = '2016-06-05T12:00:00';
             let time2 = '2016-06-05T15:00:00';
 
             // Code here
+            let time_diff = (date1, date2) => {
+                return moment(date2).to(moment(date1))
+            };
 
+            timeDiff = time_diff(time1, time2);
             expect(timeDiff).to.equal('3 hours ago');
         });
     });
